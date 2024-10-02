0d41ab4bc962cff4aa8736e687f62ec63d024daa
Wolfgang Rosner <wrosner@tirnet.de>
2024-10-02 13:31:08 +0200

    subscribe to mqtt

--- evcc_kia2p_hack.pl
+++ evcc_kia2p_hack.pl
@@ -25,13 +25,35 @@ my $debug = 4;
 
 my $mqtt_server = "homeserver.rosner.lokal";
 my $topic_evcc = "evcc";
-
 my $topic_loadpoint   = $topic_evcc   . "/loadpoints/1";
+
+#=== end of local config ==
+
 my $topic_chargePower =  $topic_loadpoint . "/chargePower";
 my $topic_minCurrent  =  $topic_loadpoint . "/minCurrent";
 my $topic_maxCurrent  =  $topic_loadpoint . "/maxCurrent";
 
-noop();
+
+my $mqtt = Net::MQTT::Simple->new($mqtt_server);
+
+debug_print(1, sprintf("Listening to MQTT server at <%s>, topic <%s>\n", 
+		$mqtt_server, $topic_loadpoint));
+
+
+#==============
+
+# __main__ MQTT subscription callback handler 
+$mqtt->run(
+        # $topic_chargePower => \&do_ecowitt,
+        $topic_loadpoint . "/#" => \&parse_default,
+        # "#" => \&noop,
+
+    );
+
+debug_print(-1, sprintf("OOPS - looks like mqtt listener died - this should not happen \n");
+
+
+# noop();
 exit;
 
 #======== subs 
@@ -39,6 +61,12 @@ exit;
 sub noop {
 	debug_print (4, "noop\n");
 }
+
+# for debug: we may parse other messages
+sub parse_default  {
+            my ($topic, $message) = @_;
+            debug_print (2, "default: [$topic] $message\n");
+        }
 
 
 # debug_print($level, $content)
