ac4f486aa4f24bdd4a915bf87b5e1ebe30f241b2
Wolfgang Rosner <wrosner@tirnet.de>
2024-10-04 15:19:06 +0200

    remove stale comments

--- evcc_kia2p_hack.pl
+++ evcc_kia2p_hack.pl
@@ -11,7 +11,9 @@
 # - getting jailed for using this piece of corrupt code
 #
 # indeed - v001 did nearly anything, at least not when it should have so
-# v002: try with state machine
+# v002            alpha test state machine
+# v003            max complexity
+# v004            alpha test moving average
 
 
 use warnings;
@@ -19,7 +21,6 @@ use strict;
 use Net::MQTT::Simple;
 
 # allow user/pwd login
-# MQTT_SIMPLE_ALLOW_INSECURE_LOGIN=1
 $ENV{"MQTT_SIMPLE_ALLOW_INSECURE_LOGIN"} = 1;
 
 use Data::Dumper;
@@ -38,15 +39,6 @@ chomp $mqtt_passwd;
 my $topic_evcc = "evcc";
 my $topic_loadpoint   = $topic_evcc   . "/loadpoints/1";
 
-
-# my $upperLimit = 5700; # 4500 ; # kW - 230 V * 8 A * 3 = 5,5 kW 
-# my $lowerLimit = 3000; # 4000 ;   # kW - 230 V * 7 A * 2 = 3,2 kW
-
-# my $cutoff = 0.5 ; # mid of the limit relative to forbidden band
-# my $hyst  =  0.2   ; # hysteresis for trigger rel to cutoff
-# my $delay = 120   ; # delay in s 
-# my $catchband =  0.8 ; # interval in A that is considered as a hit at the other end of band
-
 my $avgAvailStart = 0 ;    # start value to calc moving average
 my $avgAlpha = 0.1  ; # see https://en.wikipedia.org/wiki/Exponential_smoothing#Time_constant
 	# time contant ~ 5min
@@ -62,8 +54,6 @@ my %states_enum = (    # for check and display
   f => 'free',
   h => 'high',
   l => 'low',
-#  c => 'climbing',
-#  d => 'descending',
   i => 'idle',
   n => 'not in PV mode'
 	);
@@ -73,33 +63,6 @@ my %states_limits = (    # { lower, upper } current limits
   h => [ 8, 16 ],
   l => [ 6, 7 ]  );
 
-# ===== calculated constants
-# my %band_marks = (
-#   top    => $states_limits{f}->[1],
-#   # top_m  => '',
-#   hi_min => $states_limits{h}->[0],
-# 
-#   # ctr_up => '',
-#   # center => '',
-#   # ctr_dn => '',
-# 
-#   lo_max => $states_limits{l}->[1],
-#   # bot_m  => '',
-#   bot    => $states_limits{f}->[0],
-# );
-
-# $band_marks{center} = ($band_marks{hi_min} + $band_marks{lo_max}) /2;
-# my $bandgap = $band_marks{hi_min} - $band_marks{lo_max} ;
-# $band_marks{cutoff} = $band_marks{hi_min} +  $cutoff * $bandgap; 
-# # $band_marks{cutoff} = $band_marks{lo_max};
-# $band_marks{cut_lo} = $band_marks{cutoff} - 0.5 * $hyst * $bandgap;
-# $band_marks{cut_hi} = $band_marks{cutoff} + 0.5 * $hyst * $bandgap;
-
-# $band_marks{top_m} = $band_marks{top} - $catchband ;
-# $band_marks{bot_m} = $band_marks{bot} + $catchband ;
-
-# debug_print(4, "\%band_marks:\n" , Dumper (\%band_marks) );
-# exit;
 
 # ==== state variables
 
@@ -164,9 +127,7 @@ $mqtt->run(
   #  parse_statevar ($topic, $message, $varnam) 
 );
 
-# debug_print(-1, 
 die ("OOPS - looks like mqtt listener died - this should not happen \n");
-# noop();
 # exit;
 
 #======== subs 
@@ -198,36 +159,27 @@ sub doitnow {
     $state{state} = 'i';
   }
 
-#  # TODO -das muß runter
-#  if ($state{state} eq 'i') {
-#    if ($state{cCur}) {
-#      $state{state} ='f';
-#    } 
-#  }
-
   # - - - - - PV mode on/off - - - - -
   if ( $state{mode} ne 'pv' and $state{mode} ne 'minpv'  ) { 
     if ($state{state} eq 'n') {
       debug_print(4,"stay in mode other than PV\n"); 
-      return;
+      return; # ==== nothing to do
+
     } elsif ($state{state} eq 'i') { # transition ....
       debug_print(2, "i -> n, restoring limits\n");
-      # TODO restore limits - OK?
-      # set_current_limits('n');
       $mqtt->retain( $topic_minCurrent . '/set' => $state{min_saved}  ) ;
       $mqtt->retain( $topic_maxCurrent . '/set' => $state{max_saved}  ) ;
       $state{state} = 'n';
-      return;
+      return; # ==== restored saved current limits
+
     } else {  # leave mode over state i 
       debug_print(2, "left PV mode, switch to 'i'\n");
       $state{state} = 'i';
     }
   }
 
-  if ($state{state} eq 'n') {
+  if ($state{state} eq 'n') { # and mode is pv or minpv - see above
     debug_print(2,"n -> entering PV mode, save limits, switch to 'i'\n");
-    # TODO safe limits - OK?
-    # $states_limits{n}= [ $state{minCurrent}, $state{maxCurrent}  ];
     $state{min_saved} = $state{minCurrent};
     $state{max_saved} = $state{maxCurrent};
     debug_print(4, "  - saved - \%state:\n", Dumper(\%state));
@@ -236,7 +188,9 @@ sub doitnow {
   }
 
   
-  # TODO -das muß runter
+  # do we still need this? - not sure.... 
+  # transition i -> f -> h/l
+  # idea was to allow charger / car to choose where we start?
   if ($state{state} eq 'i') {
     if ($state{cCur}) {
       $state{state} ='f';
@@ -246,30 +200,12 @@ sub doitnow {
 
   # - - - - - core state machine  - - - - - - -
   if ($state{state} eq 'f' or $state{state} eq 'h' or $state{state} eq 'l' ) {  # handle state f='free'
-    # if ($state{phAct} == 2 or $state{cCur} <= $band_marks{lo_max}) {
     if ( $avgAvail <= $thresholdLO) {
       $state{state} = 'l';   
-    # } elsif ($state{phAct} == 3 or $state{cCur} >= $band_marks{hi_min}) { 
     } elsif ( $avgAvail >= $thresholdHI) {
       $state{state} = 'h';
     }
   }
-
-#   } elsif ($state{state} eq 'h') { # handle state h='high'
-#    if ($state{phAct} == 3 and $state{cCur} >= $band_marks{top_m}) {
-#      $state{state} = 'f';
-#    } elsif ($state{phAct} == 2 or $state{cCur} < (2/3) * $band_marks{cut_lo}) {
-#      $state{state} = 'l';
-#    }
-
-
-#  } elsif ($state{state} eq 'l') { # handle state l='low'
-#    if ($state{phAct} == 2 and $state{cCur} <= $band_marks{bot_m}) {
-#      $state{state} = 'f';
-#    } elsif ($state{phAct} == 3 or $state{cCur} > (3/2) *  $band_marks{cut_hi}) {
-#      $state{state} = 'h';
-#    }
-#  } 
   # - - - - - - - - - - - - - - - - - - - - - 
 
   debug_print(2, sprintf(" => leave: %s (%s)\n",
